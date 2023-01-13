7716d78c3e77c9276a64ceabfc38cc4200f44bfc
Matthew Butterfield <mbutterfield322@gmail.com>
2023-01-13 12:52:43 -0800

    Week 12

--- index.js
+++ index.js
@@ -34,33 +34,56 @@ function loadTable() {
   
   loadTable();
 
-  function showUserCreateBox() {
-    Swal.fire({
-      title: "Create user",
-      html:
-        '<input id="id" type="hidden">' +
-        '<input id="fname" class="swal2-input" placeholder="First">' +
-        '<input id="lname" class="swal2-input" placeholder="Last">' +
-        '<input id="username" class="swal2-input" placeholder="Username">' +
-        '<input id="email" class="swal2-input" placeholder="Email">',
-      focusConfirm: false,
-      preConfirm: () => {
-        userCreate();
-      },
-    });
+  function showUserEditBox(id) {
+    console.log(id);
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://www.mecallapi.com/api/users/" + id);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        const objects = JSON.parse(this.responseText);
+        const user = objects["user"];
+        console.log(user);
+        Swal.fire({
+          title: "Edit User",
+          html:
+            '<input id="id" type="hidden" value=' +
+            user["id"] +
+            ">" +
+            '<input id="fname" class="swal2-input" placeholder="First" value="' +
+            user["fname"] +
+            '">' +
+            '<input id="lname" class="swal2-input" placeholder="Last" value="' +
+            user["lname"] +
+            '">' +
+            '<input id="username" class="swal2-input" placeholder="Username" value="' +
+            user["username"] +
+            '">' +
+            '<input id="email" class="swal2-input" placeholder="Email" value="' +
+            user["email"] +
+            '">',
+          focusConfirm: false,
+          preConfirm: () => {
+            userEdit();
+          },
+        });
+      }
+    };
   }
   
-  function userCreate() {
+  function userEdit() {
+    const id = document.getElementById("id").value;
     const fname = document.getElementById("fname").value;
     const lname = document.getElementById("lname").value;
     const username = document.getElementById("username").value;
     const email = document.getElementById("email").value;
   
     const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "https://www.mecallapi.com/api/users/create");
+    xhttp.open("PUT", "https://www.mecallapi.com/api/users/update");
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhttp.send(
       JSON.stringify({
+        id: id,
         fname: fname,
         lname: lname,
         username: username,
